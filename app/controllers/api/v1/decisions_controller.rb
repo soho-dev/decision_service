191a5e2420bb0a8991038e7fe0e4c94e60d86385
parsym <parsym@gmail.com>
2020-04-14 02:06:07 -0700

    mocked

--- app/controllers/api/v1/decisions_controller.rb
+++ app/controllers/api/v1/decisions_controller.rb
@@ -8,10 +8,47 @@ class Api::V1::DecisionsController < Api::ApiRestrictController
   end
 
   def create
-    decision_request = DecisionRequestFilter.new(application_id, address_params, applicant_params)
-    render json: { message: "decision request not valid"}, status: 422 unless decision_request.valid?
-    decision = Processor.new(decision_request.decision_request).process
-    render json: decision.serialize
+    # decision_request = DecisionRequestFilter.new(application_id, address_params, applicant_params)
+    # render json: { message: "decision request not valid"}, status: 422 unless decision_request.valid?
+    # decision = Processor.new(decision_request.decision_request).process
+    # render json: decision.serialize
+    render json: {
+      "application_id": 123,
+        "address": {
+            "street": "123 encounter bay",
+            "city": "test_city",
+            "zip": "321",
+            "state": "test_state",
+            "county": "test_county"
+        },
+        "applicant": {
+            "first_name": "abba",
+            "last_name": "dabba",
+            "ssn": "123456789",
+            "income": 10000.0,
+            "requested_loan_amount": 20000.0
+        },
+        "final_decision": "eligible",
+        "decision": [
+            {
+                "rule_name": "mortgage_rule",
+                "decision": "eligible",
+                "message": nil
+            }
+        ],
+        "funding_options": [
+            {
+                "years": 5,
+                "interest_rate": 6,
+                "emi": 100
+            },
+            {
+                "years": 10,
+                "interest_rate": 6,
+                "emi": 60
+            }
+        ]
+    }
   end
 
   private
