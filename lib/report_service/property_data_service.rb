10f601145d7bbb6e3b0d2cad3ab298bbfeb86f7d
parsym <parsym@gmail.com>
2020-04-15 06:10:18 -0700

    adding sample reports

--- lib/report_service/property_data_service.rb
+++ lib/report_service/property_data_service.rb
@@ -4,7 +4,7 @@ module ReportService
   class PropertyDataService
     def self.call(address)
       mock_data = JSON.parse(File.read("#{Rails.root}/lib/report_service/sample_data/property.json"))
-      property_report = mock_data[address.street.parameterize.underscore]
+      property_report = mock_data[address.street.downcase.parameterize.underscore] rescue nil
       raise ReportService::ReportNotFound if property_report.nil?
       property_report
     end
